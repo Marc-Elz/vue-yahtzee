974b6d5c41da96453e7970b4df995b81032edc50
Marc-Elz <marcelzinga@live.nl>
2026-04-08 11:43:14 +0200

    Step 1: throw the dice and display it

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
-<script setup></script>
+<script setup>
+import ScoreTable from './components/ScoreTable.vue';
+import ThrownDice from './components/ThrownDice.vue';
+import {ref} from 'vue';
 
-<template></template>
+const dice = ref({
+    1: 1,
+    2: 2,
+    3: 5,
+    4: 1,
+    5: 6,
+});
+</script>
+
+<template>
+    <ThrownDice v-model="dice" />
+    <ScoreTable v-model="dice" />
+</template>
---
+++ src/components/ThrownDice.vue
@@ -0,0 +1,15 @@
+<template>
+    <li v-for="(item, index) in dice" :key="index">
+        <!-- TODO display the dicevalues nicer than this -->
+        Dice
+        {{ item }}
+    </li>
+    <button @click="throwDice()">Gooi!</button>
+</template>
+<script setup>
+const dice = defineModel();
+
+const throwDice = () => {
+    dice.value = Object.fromEntries(Object.keys(dice.value).map(key => [key, Math.floor(Math.random() * 6) + 1]));
+};
+</script>
